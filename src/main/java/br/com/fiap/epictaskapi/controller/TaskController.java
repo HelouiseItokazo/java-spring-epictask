5abca747b453ed92509879ec44547b6b56e2a98d
Leandro Ramo de Souza <leandro.ramo.ext@provu.com.br>
2022-09-07 20:11:07 -0300

    aplicação epic task para CRUD de tasks

--- src/main/java/br/com/fiap/epictaskapi/controller/TaskController.java
+++ src/main/java/br/com/fiap/epictaskapi/controller/TaskController.java
@@ -1,18 +1,13 @@
 package br.com.fiap.epictaskapi.controller;
 
-import java.util.List;
 import java.util.Optional;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -23,8 +18,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fiap.epictaskapi.model.Task;
@@ -38,20 +31,17 @@ public class TaskController {
     private TaskService service;
     
     @GetMapping
-    @Cacheable("task")
     public Page<Task> index( @PageableDefault(size = 10, sort = "title") Pageable pageable){
         return service.listAll(pageable);
     }
 
     @PostMapping
-    @CacheEvict(value="tasks", allEntries = true)
     public ResponseEntity<Task> create(@RequestBody @Valid Task task){
         service.save(task);
         return ResponseEntity.status(HttpStatus.CREATED).body(task);
     }
 
     @DeleteMapping("{id}")
-    @CacheEvict(value="tasks", allEntries = true)
     public ResponseEntity<Object> destroy(@PathVariable Long id){
         Optional<Task> optional = service.getById(id);
 
@@ -64,7 +54,6 @@ public class TaskController {
     }
 
     @GetMapping("{id}")
-    @Cacheable("task")
     public ResponseEntity<Task> show(@PathVariable Long id){
         return ResponseEntity.of( service.getById(id) );   
     }
@@ -72,7 +61,6 @@ public class TaskController {
     // Redis
 
     @PutMapping("{id}")
-    @CacheEvict(value="tasks", allEntries = true)
     public ResponseEntity<Task> update(@PathVariable Long id, @RequestBody @Valid Task newTask){
         Optional<Task> optional = service.getById(id);
 
